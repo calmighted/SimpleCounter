c343a51e29066569489aec65ca30860b200d0bfe
manish <manishbasargekar@Manishs-MacBook-Air.local>
2020-10-10 12:37:53 +0530

    initial build

---
+++ src/counter.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./index.css";
+
+
+class Counter extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+            counter: 0
+        };
+        this.handleIncr = this.handleIncr.bind(this);
+        this.handleDecr = this.handleDecr.bind(this);
+      }
+    
+    handleIncr(){
+        this.setState({
+            counter : this.state.counter + 1
+        })
+    }
+    handleDecr(){
+        this.setState({
+            counter : this.state.counter - 1
+        });
+    };
+    reset = ()=> {
+        this.setState({
+            counter: 0
+        })
+    }
+
+    render(){
+        return(
+            <div className="counterDiv">
+                <h1 className="counter">Count: {this.state.counter}</h1>
+                <button className="incr" onClick={this.handleIncr}>+</button>
+                <button className="incr" onClick={this.handleDecr}>-</button>
+                <br/>
+                <button className="reset" onClick={this.reset}>RESET</button>
+            </div>
+        );
+    }
+}
+
+
+export default Counter
